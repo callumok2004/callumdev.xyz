5e0b69cb98ee73ef8f4a90bd72cc08f558b272f7
callumok2004 <callumokane123@gmail.com>
2022-05-08 00:44:00 +0100

    Code - Linting

--- next.config.js
+++ next.config.js
@@ -6,23 +6,23 @@ module.exports = {
       {
         source: '/github',
         destination: 'https://github.com/cnrad',
-        permanent: true,
+        permanent: true
       },
       {
         source: '/twitter',
         destination: 'https://twitter.com/notcnrad',
-        permanent: true,
+        permanent: true
       },
       {
         source: '/linkedin',
         destination: 'https://linkedin.com/in/cnrad',
-        permanent: true,
+        permanent: true
       },
       {
         source: '/new',
         destination: 'https://h-cnrad.vercel.app/?pinned=https://raw.githubusercontent.com/cnrad/h.cnrad.dev/master/src/bookmarks.json&background=https://source.unsplash.com/random/1920x1080/?sunset,blue,pink',
-        permanent: true,
-      },
+        permanent: true
+      }
     ]
-  },
+  }
 }
--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,39 +1,38 @@
 import { useEffect, useState } from "react";
-import { FiSun, FiMoon } from "react-icons/fi";
+import { FiMoon, FiSun } from "react-icons/fi";
 
 const ThemeToggle = () => {
-    const [theme, setTheme] = useState<string>("dark");
+  const [theme, setTheme] = useState<string>("dark");
 
-    useEffect(() => {
-        let storedTheme = localStorage.getItem("theme") as string;
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("theme") as string;
 
-        if (!storedTheme) {
-            localStorage.setItem("theme", theme);
-        } else {
-            setTheme(storedTheme);
-            storedTheme === "dark" ? document.querySelector("html")?.classList.add("dark") : null;
-        }
-    }, []);
+    if (!storedTheme) localStorage.setItem("theme", theme);
+    else {
+      setTheme(storedTheme);
+      storedTheme === "dark" ? document.querySelector("html")?.classList.add("dark") : null;
+    }
+  }, []);
 
-    const changeTheme = (theme: string) => {
-        let newTheme = theme === "light" ? "dark" : "light";
+  const changeTheme = (theme: string) => {
+    const newTheme = theme === "light" ? "dark" : "light";
 
-        localStorage.setItem("theme", newTheme);
-        setTheme(newTheme);
-        newTheme === "light"
-            ? document.querySelector("html")?.classList.remove("dark")
-            : document.querySelector("html")?.classList.add("dark");
-    };
+    localStorage.setItem("theme", newTheme);
+    setTheme(newTheme);
+    newTheme === "light"
+      ? document.querySelector("html")?.classList.remove("dark")
+      : document.querySelector("html")?.classList.add("dark");
+  };
 
-    return (
-        <button
-            className="p-2 rounded-md bg-transparent hover:bg-black/5 dark:hover:bg-white/5 cursor-pointer"
-            onClick={() => changeTheme(theme)}
-        >
-            {theme === "light" && <FiSun className="text-black w-6 h-6 xs:w-5 xs:h-5" />}
-            {theme === "dark" && <FiMoon className="text-white w-6 h-6 xs:w-5 xs:h-5" />}
-        </button>
-    );
+  return (
+    <button
+      className="p-2 rounded-md bg-transparent hover:bg-black/5 dark:hover:bg-white/5 cursor-pointer"
+      onClick={() => changeTheme(theme)}
+    >
+      {theme === "light" && <FiSun className="text-black w-6 h-6 xs:w-5 xs:h-5" />}
+      {theme === "dark" && <FiMoon className="text-white w-6 h-6 xs:w-5 xs:h-5" />}
+    </button>
+  );
 };
 
 export default ThemeToggle;
